99ee07b8e239b47776486ad02926dc0246c7b3fd
shaman <shaman.neo@outlook.com>
2022-01-03 21:43:16 +0900

    [ATL] Init

---
+++ ATLProject/MainWnd.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "MainWnd.h"
+
+CMainWnd::CMainWnd()
+{
+
+}
+
+CMainWnd::~CMainWnd()
+{
+
+}
+
+LRESULT CMainWnd::OnCommand(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled) 
+{
+    return 0 ; 
+}
+
+LRESULT CMainWnd::OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled) 
+{
+    return 0 ; 
+}
+
+LRESULT CMainWnd::OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL &/*bHandled*/)
+{
+    PostQuitMessage(0) ; 
+    return 0 ; 
+}
